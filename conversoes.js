eec46f25b73d4779390bc81deb2b853d10478176
Bruno Merlin <82896809+BrunoMerlin013@users.noreply.github.com>
2022-08-24 02:14:49 -0300

    Add files via upload
    Segundo dia de estudos Javascript
    Noções de diferenças entre var, let e const, true or false e conversão de tipos (string e number)

---
+++ conversoes.js
@@ -0,0 +1,11 @@
+//tipo de dado
+//booleanos
+
+//conversão implícita
+
+const numero = 789;
+const numeroString = Number("789");
+//se colocar uma letra em numeroString ela retornará um NaN//
+//Number() também pode ser usado diretamente no console//
+
+console.log (numero + numeroString)
---
+++ script.js
@@ -0,0 +1,35 @@
+//var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area)
+//var area;
+
+
+//let forma = "retangulo"
+//let comprimento = 5;
+//let altura = 7;
+//let area;
+
+//if (forma === "retangulo") {
+//    area = comprimento * altura
+//} else {
+//    area = (comprimento * altura) / 2;
+//}
+//console.log(area)
+
+
+const forma = "quadrado"
+const altura = 7;
+const comprimento = 5;
+let area
+
+if (forma === "quadrado"){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
